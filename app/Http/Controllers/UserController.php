a38aeac2f44dbb59ed1c1c8cc7c5ffc89a147f97
Andres Minota <andresnotami@gmail.com>
2022-07-26 23:07:10 -0500

    🎉 Initial Commit: Creation Crud

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::latest()->get();
+
+        return view('users.index', [
+            'users' => $users
+        ]);
+
+        // $users = User::latest()->get();
+
+        // return view('welcome', [
+        //     'users' => $users
+        // ]);
+    }
+
+    public function store(Request $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+
+        return back();
+    }
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return back();
+    }
+}
